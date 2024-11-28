5cb1cda9b3f8d003de572cb5e276642bf257fd01
faiga91 <faiga.alawd91@gmail.com>
2024-11-28 22:24:21 +0100

    Diable the instance segmentation head

--- configs/common/models/mask_rcnn_vitdet.py
+++ configs/common/models/mask_rcnn_vitdet.py
@@ -1,11 +1,16 @@
 from functools import partial
+import torch
 import torch.nn as nn
 from detectron2.config import LazyCall as L
 from detectron2.modeling import ViT, SimpleFeaturePyramid
 from detectron2.modeling.backbone.fpn import LastLevelMaxPool
+from detectron2.modeling.box_regression import Box2BoxTransform
+from detectron2.modeling.roi_heads.fast_rcnn import FastRCNNOutputLayers
 
 from .mask_rcnn_fpn import model
 from ..data.constants import constants
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model.pixel_mean = constants.imagenet_rgb256_mean
 model.pixel_std = constants.imagenet_rgb256_std
@@ -49,11 +54,29 @@ model.backbone = L(SimpleFeaturePyramid)(
     square_pad=1024,
 )
 
-model.roi_heads.box_head.conv_norm = model.roi_heads.mask_head.conv_norm = "LN"
+#model.roi_heads.box_head.conv_norm = model.roi_heads.mask_head.conv_norm = "LN"
+model.roi_heads.box_head.conv_norm = "LN"
+model.roi_heads.mask_in_features = []
+model.roi_heads.mask_head = None
+
+model.roi_heads.num_classes = 1
+
+model.roi_heads.box_head.fc_dims = [512]
+
+
+box2box_transform = Box2BoxTransform(weights=(10, 10, 5, 5))
+
+model.roi_heads.box_predictor = FastRCNNOutputLayers(
+    input_shape=512, 
+    num_classes=model.roi_heads.num_classes,
+    box2box_transform=box2box_transform,
+    test_score_thresh=0.05, 
+    test_nms_thresh=0.5,
+)
 
 # 2conv in RPN:
 model.proposal_generator.head.conv_dims = [-1, -1]
 
 # 4conv1fc box head
 model.roi_heads.box_head.conv_dims = [256, 256, 256, 256]
-model.roi_heads.box_head.fc_dims = [1024]
+model.roi_heads.box_head.fc_dims = [512]
